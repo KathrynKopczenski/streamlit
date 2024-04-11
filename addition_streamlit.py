7890970d3b8f8d2eb78e3dbe48ebefafa71f12c2
KathrynKopczenski <157663004+KathrynKopczenski@users.noreply.github.com>
2024-04-11 15:53:37 -0400

    Add files via upload

--- addition_streamlit.py
+++ addition_streamlit.py
@@ -5,8 +5,9 @@ st.title("Hello")
 
 # Add a text input widget
 user_input1 = st.text_input("Enter a number:")
-user_input2 = st.text_input("Enter a number:")
-answer = user_input1 + user_input2
+
+
+
 
 # Display a greeting message
-st.write(f" {user_input1}+ {user_input2} = {answer}")
+st.write(f" {user_input1}")
